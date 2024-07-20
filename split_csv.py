7d6949837dfb60a1488d514217318bb4add10496
João Marcelo <jmsf3@cin.ufpe.br>
2024-07-20 01:02:23 -0300

    style: add title, fix formatting

--- split_csv.py
+++ split_csv.py
@@ -1,11 +1,11 @@
 import os
 import pandas as pd
 
-# Carregar o arquivo grande
+# Carregar o arquivo inteiro
 file_path = 'data/tweets.csv'
 chunk_size = 50000  # Número de linhas por parte
 
-# Ler o arquivo em partes menores
+# Dividir o arquivo em partes
 for i, chunk in enumerate(pd.read_csv(file_path, chunksize=chunk_size, encoding='ISO-8859-1')):
     chunk.to_csv(f'data/tweets_part_{i}.csv', index=False)
 
